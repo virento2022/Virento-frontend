7dd4bd3109e42139e7be7ce365d333729164429b
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-08-04 14:18:29 -0700

    Fixed redirect after post

--- components/Product/PostProduct.jsx
+++ components/Product/PostProduct.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import baseUrl from "../../utils/baseUrl";
+import Router from "next/router";
 
 const Form = () => {
     const [state, setState] = React.useState({
@@ -38,8 +39,9 @@ const Form = () => {
             .then(({ data }) => {
                 const { redirect } = data;
                 // Redirect used for reCAPTCHA and/or thank you page
-                window.location.href = redirect;
-                console.log(redirect);
+                // window.location.href = redirect;
+                // console.log(redirect);
+                Router.push("/product");
             })
             .catch((e) => {
                 window.location.href = e.response.data.redirect;
@@ -50,90 +52,93 @@ const Form = () => {
     }
 
     return (
-        <div className="block p-6 rounded-lg shadow-lg bg-white w-4/5 mx-30 mx-10 h-100">
-            <form onSubmit={handleSubmit}>
-                <div className="border-emeral d-400 border-2 rounded-2x1 text-green-600 text-xl drop-shadow-xl">
-                    <legend className="mx-32 my-4 p-1 text-3xl p-1 text-3xl text-center font-bold">
-                        Đăng tải sản phẩm
-                    </legend>
-                    <label for="title" className="">
-                        Tên Sản Phẩm
-                    </label>
-                    <br />
-                    <input
-                        name="title"
-                        type="text"
-                        placeholder="Enter title"
-                        onChange={handleChange}
-                        value={state.title}
-                        required
-                    />
-                    <br />
-                    <label for="category" className="">
-                        Tên Sản Phẩm
-                    </label>
-                    <br />
-                    <input
-                        name="category"
-                        type="text"
-                        placeholder="Enter category"
-                        onChange={handleChange}
-                        value={state.category}
-                        required
-                    />
-                    <br />
-                    <label for="price" className="">
-                        Giá cho thuê
-                    </label>
-                    <br />
-                    <input
-                        name="price"
-                        type="number"
-                        placeholder="Enter price"
-                        onChange={handleChange}
-                        value={state.price}
-                        required
-                    />
-                    <br />
-                    <label for="condition" className="">
-                        Tình trạng sản phẩm
-                    </label>
-                    <br />
-                    <input
-                        name="condition"
-                        placeholder="Enter condition"
-                        onChange={handleChange}
-                        value={state.condition}
-                        required
-                    />
-                    <br />
+      <div className="block p-6 rounded-lg shadow-lg bg-white w-4/5 mx-30 mx-10 h-100">
+        <form onSubmit={handleSubmit}>
+          <div className="border-emeral d-400 border-2 rounded-2x1 text-green-600 text-xl drop-shadow-xl">
+            <legend className="mx-32 my-4 p-1 text-3xl p-1 text-3xl text-center font-bold">
+              Đăng tải sản phẩm
+            </legend>
+            <label for="title" className="">
+              Tên Sản Phẩm
+            </label>
+            <br />
+            <input
+              name="title"
+              type="text"
+              placeholder="Enter title"
+              onChange={handleChange}
+              value={state.title}
+              required
+            />
+            <br />
+            <label for="category" className="">
+              Danh mục sản phẩm
+            </label>
+            <br />
+            <input
+              name="category"
+              type="text"
+              placeholder="Enter category"
+              onChange={handleChange}
+              value={state.category}
+              required
+            />
+            <br />
+            <label for="price" className="">
+              Giá cho thuê
+            </label>
+            <br />
+            <input
+              name="price"
+              type="number"
+              placeholder="Enter price"
+              onChange={handleChange}
+              value={state.price}
+              required
+            />
+            <br />
+            <label for="condition" className="">
+              Tình trạng sản phẩm
+            </label>
+            <br />
+            <input
+              name="condition"
+              placeholder="Enter condition"
+              onChange={handleChange}
+              value={state.condition}
+              required
+            />
+            <br />
 
-                    <label for="description" className="">
-                        Miêu tả sản phẩm
-                    </label>
-                    <br />
-                    <textarea
-                        name="description"
-                        placeholder="Enter description"
-                        onChange={handleChange}
-                        value={state.description}
-                        required
-                    />
-                    <br />
-                    <input type="file" name="file" onChange={handleChange} />
-                    <br />
-                    <input
-                        name="bot-field"
-                        type="text"
-                        onChange={handleChange}
-                        style={{ display: "none" }}
-                    />
-                    <button type = "submit" lassName="my-4 py-4 px-12 bg-green-300 text-green-800 border-2 hover:bg-green-500 hover:cursor-pointer rounded-full">
-                        Đăng sản phẩm
-                    </button>
-                </div>
-            </form>
-        </div>
+            <label for="description" className="">
+              Miêu tả sản phẩm
+            </label>
+            <br />
+            <textarea
+              name="description"
+              placeholder="Enter description"
+              onChange={handleChange}
+              value={state.description}
+              required
+            />
+            <br />
+            <input type="file" name="file" onChange={handleChange} />
+            <br />
+            <input
+              name="bot-field"
+              type="text"
+              onChange={handleChange}
+              style={{ display: "none" }}
+            />
+            <button
+              type="submit"
+              lassName="my-4 py-4 px-12 bg-green-300 text-green-800 border-2 hover:bg-green-500 hover:cursor-pointer rounded-full"
+            >
+              Đăng sản phẩm
+            </button>
+          </div>
+        </form>
+      </div>
     );
 };
 
--- pages/request.js
+++ pages/request.js
@@ -19,36 +19,34 @@ export default function Request( {requests} ) {
     console.log(requests)
 
     return (
-        <>
-            <Navbar />
-            <div className="max-w-screen-xl mx-auto">
-                <section className="relative py-20 bg-white">
-                    <div className="container px-4 mx-auto">
-                        <div className="max-w-3xl mb-20 mx-auto text-center">
-                            <span className="text-xl text-green-600 font-semibold">
-                                Request Notification
-                            </span>
-
-                        </div>
-                        <div className="flex flex-wrap -m-3 mb-16">
-                            {requests.map((request) => (
-                                <RequestCard
-                                    key={request.id}
-                                    userFrom={request.userfrom}
-                                    userTo={request.userto}
-                                    
-                                />
-                            ))}
-                        </div>
-                        <div className="text-center">
-                            <button className="px-6 py-4 text-white text-sm font-semibold bg-green-600 hover:bg-green-700 rounded transition duration-200">
-                                View More Request
-                            </button>
-                        </div>
-                    </div>
-                </section>
-                <Footer />
+      <>
+        <Navbar />
+        <div className="max-w-screen-xl mx-auto">
+          <section className="relative py-20 bg-white">
+            <div className="container px-4 mx-auto">
+              <div className="max-w-3xl mb-20 mx-auto text-center">
+                <span className="text-xl text-green-600 font-semibold">
+                  Thông báo đơn hàng
+                </span>
+              </div>
+              <div className="flex flex-wrap -m-3 mb-16">
+                {requests.map((request) => (
+                  <RequestCard
+                    key={request.id}
+                    userFrom={request.userfrom}
+                    userTo={request.userto}
+                  />
+                ))}
+              </div>
+              <div className="text-center">
+                <button className="px-6 py-4 text-white text-sm font-semibold bg-green-600 hover:bg-green-700 rounded transition duration-200">
+                  Xem thêm đơn hàng
+                </button>
+              </div>
             </div>
-        </>
+          </section>
+          <Footer />
+        </div>
+      </>
     );
 }
